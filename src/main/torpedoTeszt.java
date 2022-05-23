33a6e71ecec996e0d79818fcd8b8231a1f5a5331
janos0348 <katonajanos0128@gamil.com>
2022-05-23 12:36:49 +0200

    tesztLoves metodus kesz

--- src/main/torpedoTeszt.java
+++ src/main/torpedoTeszt.java
@@ -3,8 +3,14 @@ package main;
 public class torpedoTeszt {
     public static void main(String[] args) {
         System.out.println("Teszt");
+        // assert 3==5: "Nem jó a találat ellenőrzés";
+        
+        new torpedoTeszt().tesztLoves(4);
     }
     public String tesztLoves(int poz){
+        Hajo hajo = new Hajo();
+        String t = hajo.talalat(poz);
+        assert t.equals("talált") : "nem jó a találat ellenőrzése";
         return "";
     }
 }
