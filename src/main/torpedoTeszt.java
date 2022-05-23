8465947b83547496cb77e7e722db7a5f58210403
janos0348 <katonajanos0128@gamil.com>
2022-05-23 12:19:01 +0200

    torpedoTeszt class

---
+++ src/main/torpedoTeszt.java
@@ -0,0 +1,10 @@
+package main;
+
+public class torpedoTeszt {
+    public static void main(String[] args) {
+        System.out.println("Teszt");
+    }
+    public String tesztLoves(int poz){
+        return "";
+    }
+}
